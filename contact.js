56b0881bc371d783a404b61617f8102dfd6a51c9
Gyryk <agrawalla.girik@gmail.com>
2025-12-14 14:11:48 +0000

    Removing contact page

--- contact.js
+++ contact.js
@@ -1,31 +1,3 @@
-// Title Hover
-const letters = "[]{}()&*%^#$!@<>,./?:;\|`~'QWERTYUIOPASDFGHJKLZXCVBNM"
-
-document.querySelector('.aboutTitle').onmouseover = event => 
-{
-    iterations = 0
-    const interval = setInterval(() => 
-    {
-        event.target.innerText = event.target.innerText.split("")
-            .map((letter, index) => 
-            {  
-                if(index < iterations)
-                {
-                    return event.target.dataset.value[index];
-                }
-
-                return letters[Math.floor(Math.random() * letters.length)]
-            })
-            .join("");
-
-        if(iterations >= event.target.dataset.value.length) clearInterval(interval);
-
-        iterations += 1/4;
-    }, 40)
-
-    event.target.innerText = event.target.dataset.value;
-}
-
 // Socials Hover
 document.querySelector('.git').addEventListener('mouseover', function() {
     document.getElementById('gitImg').src = 'img/icons8-github.gif'
@@ -39,20 +11,6 @@ document.querySelector('.linked').addEventListener('mouseover', function() {
 });
 document.querySelector('.linked').addEventListener('mouseleave', function() {
     document.getElementById('linkedImg').src = 'img/icons8-linkedin.png'
-});
-
-document.querySelector('.twit').addEventListener('mouseover', function() {
-    document.getElementById('twitImg').src = 'img/icons8-twitter.gif'
-});
-document.querySelector('.twit').addEventListener('mouseleave', function() {
-    document.getElementById('twitImg').src = 'img/icons8-twitter.png'
-});
-
-document.querySelector('.insta').addEventListener('mouseover', function() {
-    document.getElementById('instaImg').src = 'img/icons8-instagram.gif'
-});
-document.querySelector('.insta').addEventListener('mouseleave', function() {
-    document.getElementById('instaImg').src = 'img/icons8-instagram.png'
 });
 
 
